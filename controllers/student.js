c298b400b093a3a12862cfd3294312b9c334b6b4
Shamim Ahamed <shamim.aiub2@gmail.com>
2020-11-21 13:00:18 +0600

    student added2

--- controllers/student.js
+++ controllers/student.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const adminModel = require.main.require('./models/adminModel');
+const studentModel = require.main.require('./models/studentModel');
 const router = express.Router();
 
 
 router.get('/',(req,res)=>{
+    if(req.session.sid != null){
+        var id=req.session.sid;
+        //console.log("session:"+id);
+
+        adminModel.getById(id,function(results){
+            var student={
+                name:results.name,
+                email:results.email,
+                gender:results.gender,
+                dob:results.dob,
+                address:results.address,
+                contact:results.contact,
+                blood:results.blood,
+                status:results.status,
+                type:results.type
+            };
+            console.log(student);
+            if(student.type==='Student' && student.status==='Active'){
+                res.render('student/portal',student);
+            }
+            else{
+                res.redirect('/login');
+            }
+
+        });
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+router.get('/GradeReport',(req,res)=>{ 
     if(req.session.sid != null){
         var id=req.session.sid;
         //console.log("session:"+id);
@@ -19,12 +51,77 @@ router.get('/',(req,res)=>{
                 status:results.status,
                 type:results.type
             };
+            
+            console.log(student);
+            if(student.type==='Student' && student.status==='Active'){
+                res.render('student/GradeReport',student);
+            }
+            else{
+                res.redirect('/portal');
+            }
+        })
+
+        
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+router.post('/GradeReport',(req,res)=>{
+    res.send(req.body.text);
+});
+
+router.get('/CoursesResult',(req,res)=>{
+    //res.render('student/CoursesResult');
+    if(req.session.sid != null){
+        var id=req.session.sid;
+        //console.log("session:"+id);
+        studentModel.getById2(id,function(results){
+            var course={
+                id:results.id,
+                courseName:results.courseName,
+                teacherName:results.teacherName,
+                grade:results.grade,
+                timing:results.timing, 
+                status:results.status   
+            }
+            console.log(course);
+            if(course.status==='Pass'){
+                res.render('student/CoursesResult',course);
+            }
+        });
+    }
+    else{
+        res.redirect('/login');
+    }
+});
+router.post('/CoursesResult',(req,res)=>{
+    res.send(req.body.text);
+});
+router.get('/Profile',(req,res)=>{
+    //res.render('student/Profile',student);
+    if(req.session.sid != null){
+        var id=req.session.sid;
+        //console.log("session:"+id);
+        adminModel.getById(id,function(results){
+            var student={
+                name:results.name,
+                username:results.username,
+                email:results.email,
+                gender:results.gender,
+                dob:results.dob,
+                address:results.address,
+                contact:results.contact,
+                blood:results.blood,
+                status:results.status,
+                type:results.type
+            };
             console.log(student);
             if(student.type==='Student' && student.status==='Active'){
                 res.render('student/Profile',student);
             }
             else{
-                res.redirect('/login');
+                res.redirect('/portal');
             }
 
         });
@@ -32,25 +129,8 @@ router.get('/',(req,res)=>{
     else{
         res.redirect('/login');
     }
-    //res.render('teacher/home');
 });
-
-
-router.get('/Course&Result',(req,res)=>{
-    res.render('student/Course&Result');
-});
-
-
-router.post('/Course&Result',(req,res)=>{
-    //res.render('teacher/notice');
-    res.send(req.body.text);
-});
-router.get('/GradeReport',(req,res)=>{
-    res.render('student/GradeReport');
-});
-
-router.post('/GradeReport',(req,res)=>{
-    //res.render('teacher/notice');
+router.post('/Profile',(req,res)=>{
     res.send(req.body.text);
 });
 module.exports =router;
--- database/portal.sql
+++ database/portal.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2020 at 10:38 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- Generation Time: Nov 21, 2020 at 07:52 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Database: `portal`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `courseName` varchar(255) NOT NULL,
+  `teacherName` varchar(255) NOT NULL,
+  `grade` varchar(255) NOT NULL,
+  `timing` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `sid` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`id`, `courseName`, `teacherName`, `grade`, `timing`, `status`, `sid`) VALUES
+(1122, 'ATP3', 'Md.Al-amin', 'A+', 'Sunday 11:00pm - 2:00pm', 'Pass', 2006);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `library`
+--
+
+CREATE TABLE `library` (
+  `bookId` int(25) NOT NULL,
+  `bookName` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `sid` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `library`
+--
+
+INSERT INTO `library` (`bookId`, `bookName`, `author`, `category`, `sid`) VALUES
+(56456, 'Advanced Learning of Webtechnology', 'Md. Mosaraf Hossain', 'CSBook', 2006);
 
 -- --------------------------------------------------------
 
@@ -48,17 +91,31 @@ CREATE TABLE `userinfo` (
 --
 
 INSERT INTO `userinfo` (`id`, `name`, `username`, `email`, `password`, `gender`, `address`, `dob`, `contact`, `blood`, `status`, `type`) VALUES
+(0, 'Shamim', '', 'Shamim.aiub2@gmail.com', '1997', 'Male', 'nikunja-2', '10-07-1997', '646464634', 'A+', 'Active', 'Student '),
 (2000, 'noman', '2020-2000', 'noman@gmail.com', '2', 'Male', 'cumilla', '03.02.1997', '01793288603', 'AB+', 'Active', 'Admin'),
 (2001, 'sakib', '2020-2001', 'nomanafd@gmail.com', 'n', 'Male', 'cumilla', '03.02.1997', '01793288603', 'AB+', 'Deactive', 'Admin'),
 (2002, 'Abdullah Al Noman', '2020-2002', 'abdullahnoman1997@gmail.com', '01639439944', 'Male', 'House 176 ,Block -C ,Road -6, Bashundara r/a', '2020-11-16', '01639439944', 'AB+', 'Active', 'Admin'),
 (2003, 'Fahim Abdullah', '2020-2003', 'fahim@gmail.com', '01793288603', 'Male', 'dhaka bangladesh', '2020-11-17', '01793288603', 'B+', 'Active', 'Admin'),
 (2004, 'Anik sikder', '2020-2004', 'sikder@gmail.com', '01622407600', 'Male', 'dhaka bangladesh', '2020-11-03', '01622407600', 'B+', 'Active', 'Admin'),
 (2005, 'Abdullah Al Noman', '2020-2005-3', 'abdullahnoman1997@gmail.com', '01793288603', 'Male', 'House 176 ,Block -C ,Road -6, Bashundara r/a', '2020-11-17', '01793288603', 'AB+', 'Active', 'Teacher'),
-(2006, 'Abdullah Al Noman', '20-2006-3', 'abdullahnoman1997@gmail.com', '01639439945', 'Male', 'House 176 ,Block -C ,Road -6, Bashundara r/a', '', '01639439945', 'A+', 'Active', 'Student');
+(2006, 'Shamim', '20-2006-3', 'shamimahamed1997@gmail.com', '1997', 'Male', 'House 176 ,Block -C ,Road -6, Bashundara r/a', '', '01639439945', 'A+', 'Active', 'Student'),
+(2007, 'Shamim', 'Shamim17', 'Shamim.aiub2@gmail.com', '1997', 'Male', 'nikunja-2', '10-07-1997', '5646464', 'A+', 'Active', 'Student ');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `library`
+--
+ALTER TABLE `library`
+  ADD PRIMARY KEY (`bookId`);
 
 --
 -- Indexes for table `userinfo`
